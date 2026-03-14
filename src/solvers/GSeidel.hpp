493e9639091b2ca62df48baaa14ecb13397a244c
mih0rel <korotkikh.me@phystech.edu>
2026-03-14 12:43:02 +0300

    Update GSeidel.hpp

--- src/solvers/GSeidel.hpp
+++ src/solvers/GSeidel.hpp
@@ -2,53 +2,35 @@
 
 #include "matrices/CSR.hpp"
 
+#include <algorithm>
 #include <vector>
 #include <cmath>
-#include <limits>
 #include <stdexcept>
 
 template<typename T>
-std::vector<T> gseidel(CSR<T>& matrix,
-                       const std::vector<T>& b,
-                       std::vector<T> x,
-                       double break_condition)
-{
-    double r_cur = std::numeric_limits<double>::infinity();
-    std::vector<double> res(x.size());
-
-    while (r_cur > break_condition) {
-        std::vector<T> x_old = x;
-
-        for (std::size_t j = 0; j < x.size(); ++j) {
+std::vector<T> gseidel( CSR<T>& matrix, const std::vector<T>& b, std::vector<T> x_0, double break_condition){
+    double r_cur = 1e8;
+    std::vector<double> res(x_0.size()); 
+    std::vector<T> x_old(x_0.size());
+    for(; r_cur > break_condition;){
+        std::copy(x_0.begin(), x_0.end(), x_old.begin());
+        for(std::size_t j = 0; j < x_0.size(); j++){
             T L = 0;
             T U = 0;
             T d = 0;
-
-            for (std::size_t k = matrix.get_rows(j); k < matrix.get_rows(j + 1); ++k) {
-                std::size_t col = matrix.get_cols(k);
-                T a = matrix.get_values(k);
-
-                if (col < j) {
-                    L += a * x[col];
-                }
-                else if (col > j) {
-                    U += a * x_old[col];
-                }
-                else {
-                    d = a;
-                }
+            for(std::size_t k = matrix.get_rows(j); k < matrix.get_rows(j + 1); k++){
+                if(matrix.get_cols(k) < j)
+                    L += matrix.get_values(k) * x_0[matrix.get_cols(k)];
+                else if(matrix.get_cols(k) > j)
+                    U += matrix.get_values(k) * x_old[matrix.get_cols(k)];
+                else
+                    d = matrix.get_values(k);
             }
-
-            if (d == 0) {
-                throw std::runtime_error("0");
-            }
-
-            x[j] = (b[j] - L - U) / d;
+            if(d == 0) throw std::runtime_error("0");
+            x_0[j] = (b[j] - L - U) / d;
         }
-
-        res = (matrix * x) - b;
-        r_cur = std::sqrt(dot(res, res));
+         res = (matrix * x_0) - b;
+         r_cur = std::sqrt(dot(res, res));
     }
-
-    return x;
+    return x_0;
 }
